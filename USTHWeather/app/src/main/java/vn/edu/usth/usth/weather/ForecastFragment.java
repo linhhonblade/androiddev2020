4838b63fb7a810e4cafacbff1cbe85a8971581e1
linhhonblade <meomaimaimeo@gmail.com>
2019-11-14 12:16:00 +0700

    fix practice 4: add TextView and ImageView without changing xml file

--- USTHWeather/app/src/main/java/vn/edu/usth/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usth/weather/ForecastFragment.java
@@ -30,31 +30,18 @@ public class ForecastFragment extends Fragment {
 
         TextView day_text = new TextView(getActivity());
         day_text.setText("Thursday");
-        day_text.setBackgroundColor(0xffffff00);
-        day_text.setPadding(0, 10, 0, 10);
+        day_text.setGravity(Gravity.CENTER);
 
         ImageView weatherIcon = new ImageView(getActivity());
         weatherIcon.setImageResource(R.drawable.rainy);
-        weatherIcon.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        weatherIcon.setScaleX((float) 0.5);
-        weatherIcon.setScaleY((float) 0.5);
-        weatherIcon.setAdjustViewBounds(true);
-        weatherIcon.setBackgroundColor(0x0000ffff);
 
-        RelativeLayout.LayoutParams textViewParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        textViewParam.addRule(RelativeLayout.CENTER_IN_PARENT);
-        RelativeLayout.LayoutParams imageViewParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-        View v = inflater.inflate(R.layout.fragment_forecast, container, false);
-        v.setBackgroundColor(0xffff0000);
-        LinearLayout linearLayout = v.findViewById(R.id.linearLayout);
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        linearLayout.setBackgroundColor(0xFF11DDCC);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setGravity(Gravity.CENTER);
-        linearLayout.addView(day_text, textViewParam);
-        linearLayout.addView(weatherIcon, imageViewParam);
+        linearLayout.addView(day_text);
+        linearLayout.addView(weatherIcon);
 
-        return v;
-//        return inflater.inflate(R.layout.fragment_forecast, container, false);
+        return linearLayout;
     }
 
 }
